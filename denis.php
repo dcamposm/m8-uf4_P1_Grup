5782e925a5a34ac301eba584824d3427c426904d
T s u k i <T s u k i@192.168.19.81>
2018-10-03 18:00:43 +0200

    error #2

--- denis.php
+++ denis.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Error error";
+            echo "Error error error";
         ?>
     </body>
 </html>
