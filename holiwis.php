0670dd51910e4a1816dc10f943f877a4b3cb8b92
T s u k i <T s u k i@192.168.19.81>
2018-10-03 17:33:42 +0200

    #2

---
+++ holiwis.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of holiwis
+ *
+ * @author T s u k i
+ */
+class holiwis {
+    //put your code here
+}
