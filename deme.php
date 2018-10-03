892478c54e6f6399b5a4c72ba73c7be54faa6428
DieCM <didaccampos@gmail.com>
2018-10-03 18:18:52 +0200

    Primer

---
+++ deme.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <?php
+        class gos{
+     
+            // Atributos
+            public $raza;
+            public $color;
+            public $tamany;
+
+            // Constructor
+            public function __construct($raza, $color, $tamany){
+                $this->raza = $raza;
+                $this->color = $color;
+                $this->tamany = $tamany;
+            }
+            
+            //Metodes
+            public function getColor(){
+
+            }
+            
+            public function setColor(){
+
+            }
+
+            public function suma(){
+
+            }
+        }
+        ?>
+    </body>
+</html>
