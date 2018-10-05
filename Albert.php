77ff2e9d2196ed0b5dc52dea6ae70f5faeacb795
T s u k i <T s u k i@192.168.19.81>
2018-10-05 15:24:35 +0200

    Denis Pereira mod#3

--- Albert.php
+++ Albert.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-         echo "esto va a dar error porfa";
+         echo "esto va a dar error porfa 2";
         ?>
     </body>
 </html>
